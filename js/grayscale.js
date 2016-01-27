80211be92a57c48778c2d9e17e5fdd50abac500a
Satwik Bebortha <satwik@tikkl.com>
2016-01-26 21:01:19 -0800

    Map Edit 2.0

--- js/grayscale.js
+++ js/grayscale.js
@@ -39,7 +39,7 @@ var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
+    map.setCenter(new google.maps.LatLng(32.8810, 117.2380));
 });
 
 function init() {
